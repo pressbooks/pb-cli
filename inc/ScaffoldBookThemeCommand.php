74c9bb164d6db6e22c96ca5109d4a2bf05555420
Ned Zimmerman <ned@bight.ca>
2016-08-16 13:52:00 -0300

    Add more template items.

--- inc/ScaffoldBookThemeCommand.php
+++ inc/ScaffoldBookThemeCommand.php
@@ -24,23 +24,23 @@ class ScaffoldBookThemeCommand {
 	 * <slug>
 	 * : Slug for the new theme.
 	 *
-	 * [--theme_name=<theme_name>]
-	 * : Name for the new theme. Defaults to <slug>.
+	 * [--theme_name=<title>]
+	 * : What to put in the 'Theme Name:' header in 'style.css'. Defaults to <slug>.
 	 *
 	 * [--description=<description>]
 	 * : Human-readable description for the theme.
 	 *
 	 * [--uri=<uri>]
-	 * : URI for the theme.
+	 * : What to put in the 'Theme URI:' header in 'style.css'.
 	 *
 	 * [--author=<author>]
-	 * : Author for the theme.
+	 * : What to put in the 'Author:' header in 'style.css'.
 	 *
 	 * [--author_uri=<author_uri>]
-	 * : Author URI for the theme.
+	 * : What to put in the 'Author URI:' header in 'style.css'.
 	 *
 	 * [--license=<license>]
-	 * : License for the theme.
+	 * : What to put in the 'License:' header in 'style.css'.
 	 * ---
 	 * default: GPL 2.0+
 	 * ---
@@ -57,13 +57,20 @@ class ScaffoldBookThemeCommand {
 	 * [--dir=<dir>]
 	 * : Specify a destination directory for the command. Defaults to the themes directory.
 	 *
+	 * [--activate]
+	 * : Activate the newly created book theme.
+	 *
+	 * [--enable-network]
+	 * : Enable the newly created book theme for the entire network.
+	 *
 	 * [--force]
 	 * : Overwrite files that already exist.
 	 *
 	 * @when before_wp_load
 	 */
 	public function theme( $args, $assoc_args ) {
-		$defaults = array(
+		$theme_slug = $args[0];
+		$assoc_args = wp_parse_args( $assoc_args, [
 			'description' => '',
 			'uri' 				=> '',
 			'author'    	=> '',
@@ -71,32 +78,63 @@ class ScaffoldBookThemeCommand {
 			'license'			=> 'GPL-2.0+',
 			'version'			=> '1.0',
 			'dir'         => '',
-		);
-		$assoc_args = array_merge( $defaults, $assoc_args );
-		$assoc_args['slug'] = $args[0];
-		$assoc_args['prefix'] = str_replace('-', '_', $args[0]);
+		] );
+
+		$assoc_args['slug'] = $theme_slug;
+		$assoc_args['theme_function_safe'] = str_replace( '-', '_', $assoc_args['slug'] );
+
 		if ( empty( $assoc_args['theme_name'] ) ) {
-			$assoc_args['theme_name'] = $args[0];
+			$assoc_args['theme_name'] = ucfirst($theme_slug);
 		}
+
 		if ( empty( $assoc_args['textdomain'] ) ) {
-			$assoc_args['textdomain'] = $args[0];
+			$assoc_args['textdomain'] = $theme_slug;
 		}
+
 		if ( ! empty( $assoc_args['dir'] ) ) {
 			$theme_dir = $assoc_args['dir'];
 		} else {
 			$theme_dir = WP_CONTENT_DIR . '/themes/' . $assoc_args['slug'];
 		}
+
+		foreach ( $assoc_args as $key => $value ) {
+			$assoc_args[ 'has_' . $key ] = ! empty( $value );
+		}
+
+		$activate = Utils\get_flag_value( $assoc_args, 'activate' );
+		$enable_network = Utils\get_flag_value( $assoc_args, 'enable-network' );
 		$force = Utils\get_flag_value( $assoc_args, 'force' );
 		$package_root = dirname( dirname( __FILE__ ) );
 		$template_path = $package_root . '/templates/';
 		$files_written = $this->create_files( array(
 			"{$theme_dir}/functions.php"  => Utils\mustache_render( "{$template_path}/functions.mustache", $assoc_args ),
 			"{$theme_dir}/style.css"			=> Utils\mustache_render( "{$template_path}/style.mustache", $assoc_args ),
+			"{$theme_dir}/assets/styles/epub/style.scss"		=> Utils\mustache_render( "{$template_path}/assets/styles/epub/style.mustache", $assoc_args ),
+			"{$theme_dir}/assets/styles/prince/style.scss"  => Utils\mustache_render( "{$template_path}/assets/styles/prince/style.mustache", $assoc_args ),
+			"{$theme_dir}/assets/styles/web/style.scss"			=> Utils\mustache_render( "{$template_path}/assets/styles/web/style.mustache", $assoc_args ),
+			"{$theme_dir}/assets/styles/components/_accessibility.scss"	=> file_get_contents( "{$template_path}/assets/styles/components/_accessibility.scss" ),
+			"{$theme_dir}/assets/styles/components/_colors.scss"				=> file_get_contents( "{$template_path}/assets/styles/components/_colors.scss" ),
+			"{$theme_dir}/assets/styles/components/_elements.scss"			=> file_get_contents( "{$template_path}/assets/styles/components/_elements.scss" ),
+			"{$theme_dir}/assets/styles/components/_media.scss"					=> file_get_contents( "{$template_path}/assets/styles/components/_media.scss" ),
+			"{$theme_dir}/assets/styles/components/_specials.scss"			=> file_get_contents( "{$template_path}/assets/styles/components/_specials.scss" ),
+			"{$theme_dir}/assets/styles/components/_structure.scss"			=> file_get_contents( "{$template_path}/assets/styles/components/_structure.scss" ),
+			"{$theme_dir}/assets/styles/components/_titles.scss"				=> file_get_contents( "{$template_path}/assets/styles/components/_titles.scss" ),
+			"{$theme_dir}/assets/styles/components/_toc.scss"						=> file_get_contents( "{$template_path}/assets/styles/components/_toc.scss" ),
+			"{$theme_dir}/assets/styles/epub/_fonts.scss"								=> file_get_contents( "{$template_path}/assets/styles/epub/_fonts.scss" ),
+			"{$theme_dir}/assets/styles/prince/_fonts.scss"							=> file_get_contents( "{$template_path}/assets/styles/prince/_fonts.scss" ),
+			"{$theme_dir}/assets/styles/web/_fonts.scss"								=> file_get_contents( "{$template_path}/assets/styles/web/_fonts.scss" ),
 		), $force );
+
 		if ( empty( $files_written ) ) {
 			WP_CLI::log( 'All theme files were skipped.' );
 		} else {
 			WP_CLI::success( "Created theme files in {$theme_dir}" );
+		}
+
+		if ( \WP_CLI\Utils\get_flag_value( $assoc_args, 'activate' ) ) {
+			WP_CLI::run_command( array( 'theme', 'activate', $theme_slug ) );
+		} else if ( \WP_CLI\Utils\get_flag_value( $assoc_args, 'enable-network' ) ) {
+			WP_CLI::run_command( array( 'theme', 'enable', $theme_slug ), array( 'network' => true ) );
 		}
 	}
 
