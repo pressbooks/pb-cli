2bbd3f35659f11831802b93ec46774a22b371514
Ned Zimmerman <ned@bight.ca>
2018-05-09 15:54:05 -0300

    Add docs.

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	title: "Pressbooks CLI",
+	description: "A suite of wp-cli commands for Pressbooks.",
+	themeConfig: {
+		repo: "pressbooks/pb-cli",
+		nav: [
+			{ link: "/#using", text: "Using" },
+			{ link: "/#installing", text: "Installing" },
+			{ link: "/#contributing", text: "Contributing" },
+			{ link: "/#support", text: "Support" }
+		]
+	}
+};
