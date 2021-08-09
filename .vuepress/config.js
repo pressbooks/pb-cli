5a2a5cf3580941c4d780a8a21bf16a148e0e0855
Steel Wagstaff <steel@pressbooks.com>
2021-08-09 12:02:09 -0700

    Update config.js
    Exclude vendor folder from static site build process. Update nav links.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,13 +1,14 @@
 module.exports = {
 	title: "Pressbooks CLI",
 	description: "A suite of wp-cli commands for Pressbooks.",
+	patterns: ['**/*.md', '**/*.vue', '!vendor/**'],
 	themeConfig: {
 		repo: "pressbooks/pb-cli",
 		nav: [
 			{ link: "/#using", text: "Using" },
 			{ link: "/#installing", text: "Installing" },
-			{ link: "/#contributing", text: "Contributing" },
-			{ link: "/#support", text: "Support" }
+			{ link: "/docs/contributing", text: "Contributing" },
+			{ link: "/docs/testing", text: "Testing" }
 		]
 	}
-};
+	
