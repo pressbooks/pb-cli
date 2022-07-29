7fa4ac926740c23b5cc014f68f94929b16000f17
Ned Zimmerman <ned@bight.dev>
2022-07-29 11:37:50 -0300

    test: try test with PR branch

--- inc/Tests/Context/FeatureContext.php
+++ inc/Tests/Context/FeatureContext.php
@@ -17,7 +17,8 @@ class FeatureContext extends \WP_CLI\Tests\Context\FeatureContext {
 		$this->install_wp();
 		$subdomains = ! empty( $subdirectory_or_subdomain ) && 'subdomain' === $subdirectory_or_subdomain ? 1 : 0;
 		$this->proc( 'wp core install-network', array( 'title' => 'WP CLI Network', 'subdomains' => $subdomains ) )->run_check();
-		$this->proc( 'wp plugin install https://github.com/pressbooks/pressbooks/archive/dev.zip' )->run_check();
+		// $this->proc( 'wp plugin install https://github.com/pressbooks/pressbooks/archive/dev.zip' )->run_check();
+		$this->proc( 'wp plugin install https://github.com/greatislander/pressbooks/archive/refs/heads/fix/add-api-isset-check-and-tests.zip' )->run_check();
 		$this->proc( 'wp theme install https://github.com/pressbooks/pressbooks-book/archive/dev.zip' )->run_check();
 		$this->proc( 'wp theme install https://github.com/pressbooks/pressbooks-aldine/archive/dev.zip' )->run_check();
 		$this->proc( 'cd wp-content/plugins/pressbooks && composer install --no-dev --optimize-autoloader && cd ../../../' )->run_check();
